1428e5c479c2d3d5160bd418d4ea09dee65f24ae
Lenovo <Lenovo@DESKTOP-T2U3IFN>
2022-08-12 20:17:20 +0530

    Commiting the project for the first time

---
+++ src/Package1/SampleJavaProgram.java
@@ -0,0 +1,36 @@
+package Package1;
+
+public class SampleJavaProgram {
+	
+
+	public static void main(String[] args) {
+		
+		String [] s1 = new String [5];
+		
+
+		s1[0]="Rohit";
+		
+		s1[1]="Jadeja";
+		
+		s1[2]="kohli";
+		
+		s1[3]="Bumrah";
+		
+		s1[4]="Rahul";
+		
+		int SizeofArray=s1.length;
+		
+		System.out.println(SizeofArray);
+		// size of index
+		
+		System.out.println(s1[2]);
+		
+		for(int i=0; i<5; i++)
+		{
+
+			System.out.println(s1[i]);
+			
+		}
+		
+	}
+}
